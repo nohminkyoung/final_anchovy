c1b25f9d7b29752b0667542ffec95e3dd656262a
wjswjdgns <wjswjdgns7@naver.com>
2022-12-15 11:57:09 +0900

    정훈 main 작업 업로드

--- anchovy/anchovy_main/views.py
+++ anchovy/anchovy_main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from anchovy_common.models import Custom_User, User_status
 
 def index(request):
-    return render(request, 'anchovy_main/index.html')
+    return render(request, 'anchovy_main/index.html', {'user':Custom_User, 'status': User_status})
+
+def cal(request):
+    return render(request, 'anchovy_main/view.html')

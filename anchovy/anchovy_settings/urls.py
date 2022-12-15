aae0aaf868ad7a748f861dedd5c8659ab87e178d
yudyudy <dbwlsier@naver.com>
2022-12-15 12:11:48 +0900

    url, view add

--- anchovy/anchovy_settings/urls.py
+++ anchovy/anchovy_settings/urls.py
@@ -5,4 +5,10 @@ from . import views #현재 폴더에서 views를 가리킨다.
 
 urlpatterns = [
     path('', views.index),
+    path('tutorial/', views.tutorial),
+    path('nickname/', views.nickname),
+    path('people/', views.people),
+    path('logout/', views.logout),
+    path('quit/', views.quit),
 ]
+
--- anchovy/anchovy_settings/views.py
+++ anchovy/anchovy_settings/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'anchovy_common/login.html')
+    return render(request, 'anchovy_settings/settings.html')
+
+def tutorial(request):
+    return render(request, 'anchovy_settings/tutorial.html')
+
+def nickname(request):
+    return render(request, 'anchovy_settings/nickname.html')
+    
+def people(request):
+    return render(request, 'anchovy_settings/people.html')
+
+def logout(request):
+    return render(request, 'anchovy_settings/logout.html')
+
+def quit(request):
+    return render(request, 'anchovy_settings/quit.html')
+

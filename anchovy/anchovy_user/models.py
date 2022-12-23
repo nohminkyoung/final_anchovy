b3fd103ecf3c357404ce3a36e7b2feca51838f5e
psjeong <sj926thwjdk@naver.com>
2022-12-23 09:51:00 +0900

    css

--- anchovy/anchovy_user/models.py
+++ anchovy/anchovy_user/models.py
@@ -5,7 +5,7 @@ from anchovy_common.models import Custom_User
 
 class Friend(models.Model):
     author = models.ForeignKey(Custom_User, on_delete=models.CASCADE)
-    fd = models.ForeignKey(Custom_User, on_delete=models.CASCADE)
+    # fd = models.ForeignKey(Custom_User, on_delete=models.CASCADE)
     username = models.CharField(max_length=150, verbose_name = '유저 아아디')
     friend_name = models.CharField(max_length=150, verbose_name = '유저의 친구 아아디')
     friend_nickname = models.CharField(max_length=150, verbose_name = '유저 친구 닉네임',default = '')

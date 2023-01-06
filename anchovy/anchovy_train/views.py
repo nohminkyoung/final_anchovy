a4fd17a35d8aa4707b4d164a03edff483cb91a72
nohminkyoung <nmk1188@naver.com>
2023-01-06 09:49:50 +0900

    데이터넘기기

--- anchovy/anchovy_train/views.py
+++ anchovy/anchovy_train/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from anchovy_train.models import Train
 from datetime import datetime
+from ast import literal_eval
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'anchovy_train/train.html')
@@ -40,9 +44,38 @@ def train_result(request):
 
 
 
+
+'''
+     // 푸쉬업 계산을 위한 함수를 전달합니다. 
+      /* 0 : nose  /  1 : left_eye_inner  /  2 : left_eye  /  3 : left_eye_outer */
+      /* 4 : right_eye_inner  /   5 : right_eye  /  6 : right_eye_outer  /  7 : left_ear */
+      /* 8 : right_ear  /  9 : mouth_left  /  10 : mouth_right  /  11 : left_shoulder */ 
+      /* 12 : right_shoulder  /  13 : left_elbow  /  14 : right_elbow  /  15 : left_wrist */
+      /* 16 : right_wrist  /  17 : left_pinky  /  18 : right_pinky  /  19 : left_index */
+      /* 20 : right_index  /  21 : left_thumb  /  22 : right_thumb  /  23 : left_hip  */
+      /* 24 : right_hip  /  25 : left_knee  /  26 : right_knee / 27 : left_ankle  */
+      /* 28 : right_ankle  /  29 : left_heel  /  30 right_heel  /  31 : left_foot_index */
+      /* 32 : right_foot_index  */
+'''
+@csrf_exempt
+def angle_cal(request):
+    lis_landmarks = request.POST.get('landmark')
+    landmarks = literal_eval(lis_landmarks[1:-1])
+
+
+    result = {'full_count': 1, 'excellent_count': 1}
+    
+
+    return HttpResponse(json.dumps({'result':result})) 
+
+
+
 def train_train(request):
     return render(request, 'anchovy_train/train_train.html')
+
 
 def train_practice(request):
     return render(request, 'anchovy_train/train_practice.html')
 
+def test(request):
+    return render(request, 'anchovy_train/test.html')
